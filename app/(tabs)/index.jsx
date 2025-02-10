f8000a6795c545ce1a9c27764b1b532b2cf6a90c
larabislem <islamlarabi0@gmail.com>
2025-02-10 23:25:39 +0100

    Modle styling

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -1,4 +1,4 @@
-import { View, StyleSheet} from 'react-native'
+import { View, StyleSheet , Button} from 'react-native'
 import React from 'react'
 import Inputgoal from './inputgoal';
 import Goals from './goalview';
@@ -21,11 +21,20 @@ const app = () => {
 const [goalarray,setgoalarray]= React.useState([]);
 const [goalinput,setgoalinput]= React.useState({});
 const [goalid, setgoalid] = React.useState(0);
+const [isvisible,setisvisible ]= React.useState(false);
+
 
 function addhandler(goal){
   setgoalid(goalid+1);
   setgoalarray(goalarray => [...goalarray,{id :goalid , text : goal}]);
+  setisvisible(false);
 }
+
+function cancel() {
+  setisvisible(false);
+  
+}
+
 function textinputhandler(text) {
   setgoalinput(text);
   
@@ -34,13 +43,20 @@ function deletehandler(id){
   setgoalarray(goalarray.filter(goal => goal.id !== id));
 }
 
+function Addgoalshandller() {
+  setisvisible(true);
+  
+}
+
   return (
     <View style={style.container}> 
 
-
+    <Button title='Add New Goals' onPress={Addgoalshandller}/>
     <Inputgoal  textinputhandler = {textinputhandler}  
     addhandler = {addhandler}
+    cancel={cancel}
     goalinput = {goalinput}
+    isvisible ={isvisible}
     />
      
     <Goals   
--- app/(tabs)/inputgoal.jsx
+++ app/(tabs)/inputgoal.jsx
@@ -1,39 +1,67 @@
 import React from "react";
-import { View , StyleSheet , Text, TextInput , Button} from "react-native";
+import { View , StyleSheet , Text, TextInput , Button , Modal,Image} from "react-native";
+import goal_img from'../../assets/images/goal.png'
 
 
 const style =StyleSheet.create({
     goal_input :
   {
-    display :'flex',
-    alignItems : 'center',
-    flexDirection:'row',
-    marginTop :20,
-    paddingLeft:20,
-    paddingRight :20,
-    flex:1
- 
-
-
+   backgroundColor:'#1e085a',
+   height:'100%',
+   display:'flex',
+   justifyContent:'center',
+   
   },
   text_input:{
     borderWidth :2,
     borderRadius :10,
     borderColor :'black',
     borderStyle :'solid',
-    paddingTop :10,
-    paddingBottom:10,
+    paddingTop :20,
+    paddingBottom:20,
     fontSize :16,
     paddingLeft:10,
-    flex:4,
+    backgroundColor :'#D3B8FF',
+    margin :10
+    
 
   },
-  button_container : {
-    flex:1,
-    marginLeft : 50,
-    borderRadius :10
+  button_container_add :{
+    backgroundColor : '#E7D9FF',
+    marginRight :20,
+    width:100,
+    height:50,
+    paddingTop :5,
+    borderRadius :6
+    
+    
+  },
+  button_container :{
+    display:'flex',
+    flexDirection:'row',
+    justifyContent:'center',
+    padding:10,
+    marginTop :25
+
+  },
+  button_container_cancel :{
+    backgroundColor : '#FF8383',
+    marginRight :20,
+    width:100,
+    height:50,
+    paddingTop :5,
+    borderRadius :6
+
+  },
+  goal_image:{
+    width:120,
+    height:120,
+    marginLeft:150,
+    marginBottom :40
+
 
   }
+
 });
 
 
@@ -41,12 +69,26 @@ const style =StyleSheet.create({
 export default function Inputgoal(props) {
 
 return(
-    <View style={style.goal_input}>
+
+   
+    <Modal visible={props.isvisible} animationType="fade" >
+     <View style={style.goal_input}>
+      <Image  source={goal_img} style={style.goal_image}/>
       <TextInput placeholder='Add goals her........' style={style.text_input} onChangeText={props.textinputhandler} >   </TextInput>
       <View style={style.button_container} >
-      <Button  title='Add' onPress={() => props.addhandler(props.goalinput)}/>
+      <View style={style.button_container_add} >
+      <Button  title='Add' onPress={() => props.addhandler(props.goalinput)} color={'black'}/>
+      </View>
+      <View style={style.button_container_cancel}>
+      <Button title="cancel" color="black" onPress={props.cancel} />
       </View>
       
      </View>
+
+      </View>
+      
+
+  </Modal>
+   
 );
 }
